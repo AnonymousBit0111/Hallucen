73748fea83babe839357a18b75459bd7939e1532
AnonymousBit <68566858+AnonymousBit0111@users.noreply.github.com>
2023-02-06 14:18:33 +0300

    removed pointless function

--- include/Hallucen/utility.h
+++ include/Hallucen/utility.h
@@ -3,7 +3,4 @@
 #include "Hallucen/GL/DrawData.h"
 #include "SDL2/SDL_rect.h"
 #include <vector>
-namespace Hallucen {
-
-std::vector<Hallucen::GL::TextureVertex> getRectVertices(SDL_Rect rect);
-}
+namespace Hallucen {}
--- src/utility.cpp
+++ src/utility.cpp
@@ -1,8 +1,5 @@
 #include "Hallucen/utility.h"
 namespace Hallucen
 {
-    std::vector<Hallucen::GL::TextureVertex> getRectVertices(SDL_Rect rect)
-    {
-        return {};
-    }
+
 }
